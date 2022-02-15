95dbca0358e3dc2e9486d7c1141cc7bd3c898638
hebaali1091994 <89750524+hebaali1091994@users.noreply.github.com>
2022-02-15 15:27:52 +0200

    PRODUCTS

---
+++ client/src/components/Product/ProductModel.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Modal from "react-modal"
+export default function ProductModel(props) {
+    const{product,closeModal}=props
+  return (
+    <Modal isOpen={product} onRequestClose={closeModal}>
+    <span className="spanModal" onClick={closeModal}>
+      &times;
+    </span>
+    <div className="product-info">
+      <img src={product.imgUrl} alt={product.title} /> 
+      <p>{product.title}</p>
+      <p>{product.desc}</p>
+
+      <p>{product.price}$</p>
+    </div>
+  </Modal>
+  )
+}
