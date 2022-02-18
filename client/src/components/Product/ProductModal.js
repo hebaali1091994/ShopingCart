a6b28a649c350c3cfb5b4875edbc088d8ef406a3
hebaali1091994 <89750524+hebaali1091994@users.noreply.github.com>
2022-02-18 22:51:36 +0200

    animate

--- client/src/components/Product/ProductModal.js
+++ client/src/components/Product/ProductModal.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import Modal from "react-modal"
-export default function ProductModal(props) {
-    const{product,closeModal}=props
+import Bounce from "react-reveal/Bounce"
+export default function ProductModel(props) {
+  const{product,closeModal}=props
   return (
+<>
     <Modal isOpen={product} onRequestClose={closeModal}>
     <span className="spanModal" onClick={closeModal}>
       &times;
     </span>
+    <Bounce left>
     <div className="product-info">
       <img src={product.imgUrl} alt={product.title} /> 
       <p>{product.title}</p>
       <p>{product.desc}</p>
 
       <p>{product.price}$</p>
-    </div>
-  </Modal>
+    </div></Bounce>
+  </Modal></>
   )
 }
